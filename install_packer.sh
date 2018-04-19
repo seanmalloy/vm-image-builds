4a62dfcf87f7392044263896a7706c539d79b026
Sean Malloy <spinelli85@gmail.com>
2018-04-19 00:48:04 -0500

    Initial add of KVM GCE image

--- install_packer.sh
+++ install_packer.sh
@@ -4,6 +4,6 @@ set -x
 set -u
 set -e
 
-curl https://releases.hashicorp.com/packer/1.2.0/packer_1.2.0_linux_amd64.zip > packer.zip
+curl https://releases.hashicorp.com/packer/1.2.0/packer_1.2.2_linux_amd64.zip > packer.zip
 unzip packer.zip
 
